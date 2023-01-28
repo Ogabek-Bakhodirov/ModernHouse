dd06d982146b5d88de876eaef85b39e51e86e2fa
Ogabek Bakhodirov <ogabek.bakhodirov@gmail.com>
2023-01-28 20:34:07 +0500

    New collection cell

---
+++ ModernHouse/Cells/InteriorValueCell.swift
@@ -0,0 +1,19 @@
+//
+//  InteriorValueCell.swift
+//  ModernHouse
+//
+//  Created by Ogabek Bakhodirov on 28/01/23.
+//
+
+import UIKit
+
+class InteriorValueCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
